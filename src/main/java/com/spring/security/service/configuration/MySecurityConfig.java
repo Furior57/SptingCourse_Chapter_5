139e72a45160d4dfa58ede0cdc774071aa4afb64
Furior57 <Furior572@gmail.com>
2022-09-16 15:57:55 +0300

    16.09.22_SpringSecurity_Authorization

--- src/main/java/com/spring/security/service/configuration/MySecurityConfig.java
+++ src/main/java/com/spring/security/service/configuration/MySecurityConfig.java
@@ -1,6 +1,7 @@
 package com.spring.security.service.configuration;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -23,10 +24,32 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
         // со всеми вытекающими, непосредственно при выполнении программы.
         // На этом настройка проекта, пока, завершена./
         auth.inMemoryAuthentication().withUser(userBuilder.username("Art").password("Art")
-                .roles("Employee"));
+                .roles("EMPLOYEE"));
         auth.inMemoryAuthentication().withUser(userBuilder.username("Ivan").password("Ivan")
                 .roles("HR"));
         auth.inMemoryAuthentication().withUser(userBuilder.username("Egor").password("Egor")
-                .roles("Sales"));
+                .roles("MANAGER"));
+    }
+    // Имплементируем метод configure() который принимает HttpSecurity, этот объект
+    // занимается тем, что дает разрешения перейти по url в зависимости от роли пользователя
+    // котроый запрашивает этот переход. Перейдем ниже к реализации, чтобы увидеть
+    // как это работает
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // Такой записью мы указываем, что к стартовому view у нас имеют доступ такие то роли.
+        // Можем не заполнять это поле и тогда любой пользователь будет иметь доступ
+        // к этой странице, далее мы можем продолжить настройку доступа не начиная новое
+        // выражение, прописываем новый адрес с помощью antMatchers() и методом hasRole()
+        // устанавливаем туда доступ для HR, тут уже скобки пустыми оставить не получится.
+        // В конце указываем такую конструкцию and().formLogin().permitAll(), она означает,
+        // что форма логина-пароля будет запрашиваться у всех посетителей.
+        http.authorizeHttpRequests().antMatchers("/").hasAnyRole("EMPLOYEE",
+                "HR", "SALES", "MANAGER").antMatchers("/hr_info").hasRole("HR")
+                .antMatchers("/manager_info").hasRole("MANAGER")
+                .and().formLogin().permitAll();
+        // Однако то как мы сейчас реализовали наше представление не есть хорошо, зачем
+        // ограничивать доступ к кнопке по роли, но оставлять ее видимой для всех.
+        // Мы вполне себе можем скрыть эту кнопку для всех ролей кроме целевой.
+        // Перейдем во view_for_all_employees на 10 строку и посмотрим как мы это реализуем.
     }
 }
--- src/main/webapp/WEB-INF/view/view_for_all_employees.jsp
+++ src/main/webapp/WEB-INF/view/view_for_all_employees.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="security" uri="http://www.springframework.org/security/tags" %>
 <!DOCTYPE html>
 <html>
 <body>
@@ -7,14 +8,22 @@ getInfoOnlyForHR(), и укажем его адрес как /hr_info, этот 
 несуществующий view "for_hr"--%>
 <h3>Information for all employees</h3>
 <br><br>
+<%--Этот комментарий относится к 68 лекции. Чтобы настроить доступ к кнопке по ролям
+нам необходимо добавить зависимость Spring security taglibs, а ниже мы прячем нашу кнопку
+поместив ее в контейнер security:authorize, где в параметре accsess указываем какая именно
+роль имеет доступ, роль указываем в одинарных кавычках./--%>
+<security:authorize access="hasRole('HR')" >
 <input type="button" value="Salary" onclick="window.location.href = 'hr_info'">
 Only for HR staff
+</security:authorize>
 <br><br>
 <%--Теперь по аналогии создадим кнопку только для менеджеров, точно так же определим в контроллере
 новый метод для ссылки которую мы здесь передаем. Этот метод будет возвращать view "for_managers"
  Теперь создадим оба view для возврата, логики пока в них никакой не будет, они просто имитируют
  какой то возврат, в них будет просто информационное сообщение./--%>
+<security:authorize access="hasRole('MANAGER')">
 <input type="button" value="Performance" onclick="window.location.href = 'manager_info'">
 Only for managers
+</security:authorize>
 </body>
 </html>
